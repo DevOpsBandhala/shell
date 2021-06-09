547f2167ff9d88e21545fbc9e399c64234af77cc
DevOpsBandhala <devopsbandhal@gmail.com>
2021-06-09 23:57:13 +0530

    commit

--- main.sh
+++ main.sh
@@ -1,11 +1,11 @@
  #!/bin/bash
 now=`date -u -d"+8 hour" +'%Y-%m-%d %H:%M:%S'`
 #cpu use threshold
-cpu_warn='75'
+cpu_warn='5'
 #mem idle threshold
-mem_warn='100'
+mem_warn='5'
 #disk use threshold
-disk_warn='90'
+disk_warn='5'
 #clear old log file
  >/tmp/capa.log
 #email errot count flag
@@ -64,13 +64,25 @@ if [ $disk_use -gt $disk_warn ]
 fi
 }
 
+
+item_disk_loop()
+{
+   df -mP | grep /dev | grep -v -E '(tmp|boot)' | awk '{print $1, $4, $5}' | cut -f 1 -d "%" |sort -r -n | \
+   while read -r fs_name aval_space used_space; do if test $disk_warn -lt $used_space; then \
+         echo "$now current $fs_name utilized $used_space % of space and $aval_space MB space only available - FAIL">> /tmp/capa.log else \
+         echo "$now current $fs_name utilized $used_space % of space and $aval_space MB space are available - PASS">> /tmp/capa.log  fi; \
+    done;
+}
+
 #--- main function
 item_cpu
 item_mem
-item_disk
+#item_disk
+item_disk_loop
 
 if [ $EMAIL_FALG_COUNT -gt 0 ] 
     then
+            cat /tmp/capa.log
             #--- email configuration part
             SUBJECT="Resource alert - daily "
             RECEIVER="YOUR EMIAL" 
