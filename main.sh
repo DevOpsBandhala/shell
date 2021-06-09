d30bf957b3d6e67d678044248b97124f4a5da89a
DevOpsBandhala <devopsbandhal@gmail.com>
2021-06-10 00:39:11 +0530

    commit

--- main.sh
+++ main.sh
@@ -1,16 +1,13 @@
 #!/bin/bash
 now=`date -u -d"+8 hour" +'%Y-%m-%d %H:%M:%S'`
 #cpu use threshold
-cpu_warn='5'
+cpu_warn='75'
 #mem idle threshold
-mem_warn='5'
+mem_warn='95'
 #disk use threshold
-disk_warn='5'
+disk_warn='20'
 #clear old log file
  >/tmp/capa.log
-#email errot count flag
-EMAIL_FALG_COUNT=0
-
 
 #---cpu
 item_cpu () {
@@ -19,13 +16,12 @@ cpu_use=`expr 100 - $cpu_idle`
  
 if [ $cpu_use -gt $cpu_warn ]
     then
-       echo "$now current cpu utilization rate of $cpu_use - FAIL ">> /tmp/capa.log
-       let "EMAIL_FALG_COUNT=EMAIL_FALG_COUNT+1";
-        #echo "cpu warning!!!"
-    else
-       echo "$now current cpu utilization rate of $cpu_use - PASS" >> /tmp/capa.log
+       echo "$now current cpu utilization rate of $cpu_use - FAIL ">> /tmp/capa.log;
+       
+ else
+       echo "$now current cpu utilization rate of $cpu_use - PASS" >> /tmp/capa.log;
         #echo "cpu ok!!!"
-        let "EMAIL_FALG_COUNT=EMAIL_FALG_COUNT+0";
+       
 fi
 }
 
@@ -38,11 +34,11 @@ if [ $mem_free -lt $mem_warn  ]
     then
         echo "$now the current memory space remaining ${mem_free} MB - FAIL" >> /tmp/capa.log
         #echo "mem warning!!!"
-        let "EMAIL_FALG_COUNT=EMAIL_FALG_COUNT+1";
+        
     else
        # echo "mem ok!!!"
         echo "$now the current memory space remaining ${mem_free} MB - PASS" >> /tmp/capa.log
-        let "EMAIL_FALG_COUNT=EMAIL_FALG_COUNT+0";
+        
 fi
 }
 
@@ -56,11 +52,11 @@ if [ $disk_use -gt $disk_warn ]
     then
         #echo "disk warning!!!"
         echo "$now current disk usage is and $disk_path - FAIL">> /tmp/capa.log
-        let "EMAIL_FALG_COUNT=EMAIL_FALG_COUNT+1";
+        
     else
         #echo "disk ok!!!"
         echo "$now current disk usage is and $disk_path - PASS">> /tmp/capa.log
-        let "EMAIL_FALG_COUNT=EMAIL_FALG_COUNT+0";
+        
 fi
 }
 
@@ -69,10 +65,10 @@ item_disk_loop()
 {
    df -mP | grep /dev | grep -v -E '(tmp|boot)' | awk '{print $1, $4, $5}' | cut -f 1 -d "%" |sort -r -n | \
    while read -r fs_name aval_space used_space; do if test $disk_warn -lt $used_space; then \
-         echo "$now current $fs_name utilized $used_space % of space and $aval_space MB space only available - FAIL">> /tmp/capa.log \
-         let "EMAIL_FALG_COUNT=EMAIL_FALG_COUNT+1";else \
-         echo "$now current $fs_name utilized $used_space % of space and $aval_space MB space are available - PASS">> /tmp/capa.log \
-         let "EMAIL_FALG_COUNT=EMAIL_FALG_COUNT+0"; fi; \
+         echo "$now current $fs_name utilized $used_space % of space and $aval_space MB space only available - FAIL">> /tmp/capa.log ;\
+         else \
+         echo "$now current $fs_name utilized $used_space % of space and $aval_space MB space are available - PASS">> /tmp/capa.log ;\
+          fi; \
     done;
 }
 
@@ -82,23 +78,23 @@ item_mem
 #item_disk
 item_disk_loop
 
-if [ $EMAIL_FALG_COUNT -gt 0 ] 
-    then
-            cat /tmp/capa.log
-            #--- email configuration part
-            SUBJECT="Resource alert - daily "
-            RECEIVER="YOUR EMIAL" 
-            TEXT=`cat /tmp/capa.log` 
-
-            SERVER_NAME=$HOSTNAME  
-            SENDER=$(whoami)  
-            USER="noreply"
-
-            MAIL_TXT="Subject: $SUBJECT\nFrom: $SENDER\nTo: $RECEIVER\n\n$TEXT"  
-            echo -e $MAIL_TXT | sendmail -t 
-            exit $?;
-    else
-            cat /tmp/capa.log
-            #echo $EMAIL_FALG_COUNT;
-            exit
+#Check if any FAIL on the log then email will send.
+cat /tmp/capa.log | grep 'FAIL' > /dev/null 2>&1
+if [ $? -eq 0 ]; then
+    cat /tmp/capa.log
+    #--- email configuration part
+    SUBJECT="Resource alert - daily "
+    RECEIVER="YOUR EMIAL" 
+    TEXT=`cat /tmp/capa.log` 
+    
+    SERVER_NAME=$HOSTNAME  
+    SENDER=$(whoami)  
+    USER="noreply"
+    
+    MAIL_TXT="Subject: $SUBJECT\nFrom: $SENDER\nTo: $RECEIVER\n\n$TEXT"  
+    echo -e $MAIL_TXT | sendmail -t 
+    exit $?;
+else
+    cat /tmp/capa.log      
+    exit
 fi
